c0a9c992f474ef83592ea8116a1ca2a09d7e5fb1
CheoR <cheo.roman+github@gmail.com>
2021-05-26 10:37:22 -0500

    doc:  COUNTs the number of line items for each invoiceid

---
+++ line_items_per_invoice.sql
@@ -0,0 +1,8 @@
+/*
+ Looking at the InvoiceLine table, provide a query that COUNTs 
+ the number of line items for each Invoice. HINT: GROUP BY
+ */
+SELECT InvoiceId,
+ COUNT(*)
+FROM InvoiceLine
+GROUP BY InvoiceId
