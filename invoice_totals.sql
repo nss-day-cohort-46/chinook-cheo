6afe237e18b1adf31d5208953cdb835d31859df4
CheoR <cheo.roman+github@gmail.com>
2021-05-26 09:27:00 -0500

    doc: Invoice Total, Customer name, Country and Sale Agent name  for all invoices and customers.

--- invoice_totals.sql
+++ invoice_totals.sql
@@ -2,19 +2,20 @@
  Provide a query that shows the 
  Invoice Total, Customer name, Country and Sale Agent name 
  for all invoices and customers.
- 
  */
-SELECT *
-FROM (
-  SELECT e.EmployeeId,
-   e.LastName AS EmployeeLastName,
-   e.FirstName AS EmployeeFirstName,
-   e.Title As EmployeeTitle,
-   c.CustomerId,
-   c.LastName AS CustomerLastName,
-   c.FirstName AS CustomerFirstName
-  FROM Employee e
-   INNER JOIN Customer c ON c.SupportRepId = e.EmployeeId;
-) AS Result
-LEFT JOIN Invoice i
-WHERE i.CustomerId = Result.CustomerId;
+SELECT e.EmployeeId,
+  e.LastName AS EmployeeLastName,
+  e.FirstName AS EmployeeFirstName,
+  i.InvoiceId,
+  i.BillingCountry,
+  SUM(i.Total),
+  c.CustomerId,
+  c.LastName AS CustomerLastName,
+  c.FirstName AS CustomerFirstName
+FROM EMPLOYEE e
+  INNER JOIN Customer c ON e.EmployeeId = c.SupportRepId
+  INNER JOIN Invoice i ON c.CustomerId = i.CustomerId
+GROUP BY c.CustomerId
+ORDER BY BillingCountry,
+  CustomerLastName,
+  EmployeeLastName;
