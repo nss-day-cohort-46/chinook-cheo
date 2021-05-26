4833a392cbdcc8b8c4770929990ab1cdb3e851d3
CheoR <cheo.roman+github@gmail.com>
2021-05-26 10:56:25 -0500

    doc: fetch number of invioces per country.

--- line_item_track_artist.sql
+++ line_item_track_artist.sql
@@ -1,14 +1,9 @@
 /*
- Provide a query that includes the 
- purchased track name 
- AND artist name 
- with each invoice line item.
+ Provide a query that 
+ shows the # of invoices per country. 
+ HINT: GROUP BY
  */
-SELECT i.InvoiceId,
- t.Name,
- s.Name
-FROM Invoice i
- INNER JOIN InvoiceLine l ON i.InvoiceId = l.InvoiceId
- INNER JOIN Track t ON t.TrackId = l.TrackId
- INNER JOIN Album a ON t.AlbumId = a.AlbumId
- INNER JOIN Artist s ON a.ArtistId = s.ArtistId;
+SELECT BillingCountry,
+ COUNT(*)
+FROM Invoice
+GROUP BY BillingCountry;
